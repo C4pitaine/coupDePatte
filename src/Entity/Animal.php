52613721f77f8ab30234255e760936d8ca8ef7a6
Alex <sacre.alexandre@hotmail.com>
2024-08-03 21:13:13 +0200

    Entité Image

--- src/Entity/Animal.php
+++ src/Entity/Animal.php
@@ -52,10 +52,17 @@ class Animal
     #[ORM\ManyToMany(targetEntity: Friandise::class, inversedBy: 'animals')]
     private Collection $friandise;
 
+    /**
+     * @var Collection<int, Image>
+     */
+    #[ORM\OneToMany(targetEntity: Image::class, mappedBy: 'animal', orphanRemoval: true)]
+    private Collection $images;
+
     public function __construct()
     {
         $this->suivis = new ArrayCollection();
         $this->friandise = new ArrayCollection();
+        $this->images = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -209,6 +216,36 @@ class Animal
     public function removeFriandise(Friandise $friandise): static
     {
         $this->friandise->removeElement($friandise);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Image>
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image): static
+    {
+        if (!$this->images->contains($image)) {
+            $this->images->add($image);
+            $image->setAnimal($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): static
+    {
+        if ($this->images->removeElement($image)) {
+            // set the owning side to null (unless already changed)
+            if ($image->getAnimal() === $this) {
+                $image->setAnimal(null);
+            }
+        }
 
         return $this;
     }
