61235b5b468f5ff6b36f0f6e1a76185708ec6c1b
Alex <sacre.alexandre@hotmail.com>
2024-08-07 13:39:58 +0200

    Header

--- assets/app.js
+++ assets/app.js
@@ -10,4 +10,36 @@ import './app.scss';
 import './styles/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// Gestion Menu Burger
+const menuBurger = document.querySelector('#burger')
+const header = document.querySelector('header')
+const menu = document.querySelector('#menu')
+const menuA = document.querySelectorAll('#menu a')
+const chevronMenu = document.querySelectorAll('.chevronMenu')
+const menuLink = document.querySelectorAll('.menuLink .link')
+
+menuBurger.addEventListener('click',()=>{
+    header.classList.toggle('menuOpened')
+    menu.classList.toggle('menuOpened')
+    chevronMenu.forEach((chevron)=>{
+        chevron.classList.remove('chevronOpen')
+    })
+    menuLink.forEach((link)=>{
+        link.classList.remove('linkOpen')
+    })
+})
+
+menuA.forEach((menuA)=>{
+    menu.classList.remove('menuOpened')
+    menuA.addEventListener('click',()=>{
+        menu.classList.remove('menuOpened')
+        header.classList.remove('menuOpened')
+    })
+})
+
+chevronMenu.forEach((chevron,key)=>{
+    chevron.addEventListener('click',()=>{
+        chevron.classList.toggle('chevronOpen')
+        menuLink[key].classList.toggle('linkOpen')
+    })
+})
