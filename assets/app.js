cd3f66d0f9b24f95d05072378b506eadd39f16c3
Alex <sacre.alexandre@hotmail.com>
2024-08-22 11:04:27 +0200

    Ajout du nom de l'animal dans la liste d'achat

--- assets/app.js
+++ assets/app.js
@@ -65,3 +65,40 @@ annulerDelete.forEach((annulerDelete,key)=>{
         alertDelete[key].classList.remove('active')
     })
 })
+
+var numberAchatHeader = document.querySelectorAll('.numberAchatHeader')
+var friandises = document.querySelectorAll('.friandise')
+var achat = localStorage.getItem('listCart') ? JSON.parse(localStorage.getItem('listCart')) : []
+
+
+/* Affichage du nombre de friandises dans le panier */
+function numberCart() {
+    if(localStorage.getItem('numberAchat'))
+    {
+       numberAchatHeader.forEach((item,key)=>{
+        item.innerText = localStorage.getItem('numberAchat')
+       })
+    }
+}
+numberCart()
+
+/* Ajout au panier */
+var addToCart = document.querySelectorAll('.addToCart')
+
+addToCart.forEach((cart,key)=>{
+    cart.addEventListener('click',()=>{
+        var numberAchat = localStorage.getItem('numberAchat') ? localStorage.getItem('numberAchat') : 0
+        numberAchat= parseInt(numberAchat) + 1
+        localStorage.setItem('numberAchat',numberAchat)
+        numberCart()
+
+        var friandise = {
+            animal: document.querySelector('.animalName').innerText,
+            name : friandises[key].querySelector('.friandiseName').innerText,
+            price : friandises[key].querySelector('.friandisePrice').innerText
+        }
+
+        achat.push(friandise)
+        localStorage.setItem('listCart',JSON.stringify(achat))
+    })
+})
