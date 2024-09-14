429ad4fcf01a109a2d2c7b6cabc65f4e7639a3af
Alex <sacre.alexandre@hotmail.com>
2024-09-14 13:50:22 +0200

    Dashboard

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\AdoptionRepository;
+use App\Repository\ContactRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class AdminDashboardController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'admin_dashboard_index')]
-    public function index(): Response
+    public function index(ContactRepository $contactRepo,AdoptionRepository $adoptionRepo,UserRepository $userRepo): Response
     {
+        $messageNotSeen = $contactRepo->findBy(['status'=>'en attente']);
+        $adoption = $adoptionRepo->findAll();
+        $user = $userRepo->findAll();
+
         return $this->render('admin/dashboard/index.html.twig', [
-            
+            'messageNotSeen' => Count($messageNotSeen),
+            'adoption' => Count($adoption),
+            'user' => Count($user)-1,
         ]);
     }
 }
