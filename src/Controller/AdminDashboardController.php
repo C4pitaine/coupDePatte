a86eb2684496e685c5945675eb26d86f3396959a
Alex <sacre.alexandre@hotmail.com>
2024-09-14 14:56:33 +0200

    Affichage des montants récoltés sur le dashboard

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\AdoptionRepository;
+use App\Repository\CartRepository;
 use App\Repository\ContactRepository;
+use App\Repository\DonationRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,16 +14,31 @@ use Symfony\Component\Routing\Attribute\Route;
 class AdminDashboardController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'admin_dashboard_index')]
-    public function index(ContactRepository $contactRepo,AdoptionRepository $adoptionRepo,UserRepository $userRepo): Response
+    public function index(ContactRepository $contactRepo,AdoptionRepository $adoptionRepo,UserRepository $userRepo,DonationRepository $donationRepo,CartRepository $cartRepo): Response
     {
         $messageNotSeen = $contactRepo->findBy(['status'=>'en attente']);
         $adoption = $adoptionRepo->findAll();
         $user = $userRepo->findAll();
+       
+        $donations = $donationRepo->findBy(['status'=>'payé']);
+        $totalDonation = 0;
+        foreach($donations as $donation){
+            $totalDonation+=  $donation->getMontant();
+        }
+
+        $carts = $cartRepo->findBy(['status'=>'payé']);
+        $totalCart = 0;
+        foreach($carts as $cart){
+            $totalCart+= $cart->getTotal();
+        }
+
 
         return $this->render('admin/dashboard/index.html.twig', [
             'messageNotSeen' => Count($messageNotSeen),
             'adoption' => Count($adoption),
             'user' => Count($user)-1,
+            'totalDonation' => $totalDonation,
+            'totalCart' => $totalCart,
         ]);
     }
 }
