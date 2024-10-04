69ccb676b1c2f1a78afb45dac3476d2750c33eb5
Alex <sacre.alexandre@hotmail.com>
2024-10-04 21:04:57 +0200

    Correction conflit évènement mouseover et click sur les chromium

--- assets/js/favoris.js
+++ assets/js/favoris.js
@@ -6,17 +6,13 @@ favoris.forEach((favori,index)=>{
 
     favori.setAttribute('open',false)
     favori.addEventListener('click',()=>{
-        let open = favori.getAttribute('open')
-        if(open != "true"){
-            favori.setAttribute('open',true)
-            show[index].classList.add('active')
-        }
+        show[index].classList.toggle('active')
     })
 
-    favori.addEventListener('mouseover',()=>{
-        show[index].classList.add('active')
+    favori.addEventListener('pointerenter',()=>{
+        show[index].classList.add('activeHover')
     })
-    favori.addEventListener('mouseout',()=>{
-        show[index].classList.remove('active')
+    favori.addEventListener('pointerleave',()=>{
+        show[index].classList.remove('activeHover')
     })
 })
